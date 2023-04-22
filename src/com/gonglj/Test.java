13ee79d791968157e881cefab2ba578731f6ca4f
1745479014 <97824473+1745479014@users.noreply.github.com>
2023-04-22 08:29:42 +0800

    Update Test.java

--- src/com/gonglj/Test.java
+++ src/com/gonglj/Test.java
@@ -2,6 +2,6 @@ package com.gonglj;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("魔前一跪三千年，回首凡尘不做仙");
+        System.out.println("魔前一跪");
     }
 }
